ed164ed7010a5e54be9c89b85f37207423ae29de
happymarcker <99589643+happymarcker@users.noreply.github.com>
2022-02-20 06:56:02 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,108 +1,115 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        Départ = input.runningTime()
-        basic.showString("D")
-        D.showColor(neopixel.colors(NeoPixelColors.Green))
-        D.show()
-    }
     if (receivedNumber == 2) {
-        Nombre_de_bonus += 1
+        BonusNombre += 1
         basic.showNumber(1)
-        BT1.showColor(neopixel.colors(NeoPixelColors.Green))
-        BT1.show()
+        Led02.showColor(neopixel.colors(NeoPixelColors.Green))
+        Led02.show()
     }
     if (receivedNumber == 3) {
-        Nombre_de_bonus += 1
+        BonusNombre += 1
         basic.showNumber(2)
-        BT2.showColor(neopixel.colors(NeoPixelColors.Green))
-        BT2.show()
+        Led03.showColor(neopixel.colors(NeoPixelColors.Green))
+        Led03.show()
     }
     if (receivedNumber == 4) {
-        Nombre_de_bonus += 1
+        BonusNombre += 1
         basic.showNumber(3)
-        BT3.showColor(neopixel.colors(NeoPixelColors.Green))
-        BT3.show()
+        Led04.showColor(neopixel.colors(NeoPixelColors.Green))
+        Led04.show()
     }
-    if (receivedNumber == 5) {
+    if (receivedNumber == 0) {
         Temps = (input.runningTime() - Départ) / 1000
-        Temps = Temps + (Nombre_de_bonus * Temps_Bonus - (Nombre_de_pénalités + Temps_pénalité))
+        Temps = Temps + (BonusNombre * BonusTemps - (FautesNombre + FautesTemps))
         basic.showString("F")
-        F.showColor(neopixel.colors(NeoPixelColors.Green))
-        F.show()
+        Led05.showColor(neopixel.colors(NeoPixelColors.Green))
+        Led05.show()
         basic.showString("" + (Temps))
     }
 })
 input.onButtonPressed(Button.A, function () {
-    Nombre_de_pénalités += 1
+    FautesNombre += 1
     basic.showIcon(IconNames.No)
     basic.pause(100)
     basic.clearScreen()
-    if (Nombre_de_pénalités == 1) {
-        P1.showColor(neopixel.colors(NeoPixelColors.Red))
-        P1.show()
+    if (FautesNombre == 1) {
+        Led10.showColor(neopixel.colors(NeoPixelColors.Red))
+        Led10.show()
     }
-    if (Nombre_de_pénalités == 2) {
-        P2.showColor(neopixel.colors(NeoPixelColors.Red))
-        P2.show()
+    if (FautesNombre == 2) {
+        Led09.showColor(neopixel.colors(NeoPixelColors.Red))
+        Led09.show()
     }
-    if (Nombre_de_pénalités == 3) {
-        P3.showColor(neopixel.colors(NeoPixelColors.Red))
-        P3.show()
+    if (FautesNombre == 3) {
+        Led08.showColor(neopixel.colors(NeoPixelColors.Red))
+        Led08.show()
     }
-    if (Nombre_de_pénalités == 4) {
-        P4.showColor(neopixel.colors(NeoPixelColors.Red))
-        P4.show()
+    if (FautesNombre == 4) {
+        Led07.showColor(neopixel.colors(NeoPixelColors.Red))
+        Led07.show()
     }
-    if (Nombre_de_pénalités == 5) {
-        P5.showColor(neopixel.colors(NeoPixelColors.Red))
-        P5.show()
+    if (FautesNombre == 5) {
+        Led06.showColor(neopixel.colors(NeoPixelColors.Red))
+        Led06.show()
+    }
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Go") {
+        Départ = input.runningTime()
+        basic.showString("D")
+        Led01.showColor(neopixel.colors(NeoPixelColors.Green))
+        Led01.show()
+    }
+    if (receivedString == "Faute") {
+    	
     }
 })
 input.onButtonPressed(Button.B, function () {
     Temps = 0
-    Nombre_de_bonus = 0
-    Nombre_de_pénalités = 0
+    BonusNombre = 0
+    FautesNombre = 0
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
     strip.show()
 })
 let Départ = 0
-let P5: neopixel.Strip = null
-let P4: neopixel.Strip = null
-let P3: neopixel.Strip = null
-let P2: neopixel.Strip = null
-let P1: neopixel.Strip = null
-let F: neopixel.Strip = null
-let BT3: neopixel.Strip = null
-let BT2: neopixel.Strip = null
-let BT1: neopixel.Strip = null
-let D: neopixel.Strip = null
+let Led06: neopixel.Strip = null
+let Led07: neopixel.Strip = null
+let Led08: neopixel.Strip = null
+let Led09: neopixel.Strip = null
+let Led10: neopixel.Strip = null
+let Led05: neopixel.Strip = null
+let Led04: neopixel.Strip = null
+let Led03: neopixel.Strip = null
+let Led02: neopixel.Strip = null
+let Led01: neopixel.Strip = null
 let strip: neopixel.Strip = null
 let Temps = 0
-let Nombre_de_bonus = 0
-let Temps_Bonus = 0
-let Nombre_de_pénalités = 0
-let Temps_pénalité = 0
+let BonusNombre = 0
+let BonusTemps = 0
+let FautesNombre = 0
+let FautesTemps = 0
 radio.setGroup(33)
 music.setBuiltInSpeakerEnabled(true)
 music.setVolume(154)
-basic.showIcon(IconNames.Happy)
-basic.pause(100)
-basic.clearScreen()
-Temps_pénalité = 20
-Nombre_de_pénalités = 0
-Temps_Bonus = 20
-Nombre_de_bonus = 0
+basic.showIcon(IconNames.House)
+FautesTemps = 20
+FautesNombre = 0
+let FautesMax = 5
+BonusTemps = 20
+BonusNombre = 0
 Temps = 0
 strip = neopixel.create(DigitalPin.P2, 10, NeoPixelMode.RGB)
-D = strip.range(0, 1)
-BT1 = strip.range(1, 1)
-BT2 = strip.range(2, 1)
-BT3 = strip.range(3, 1)
-F = strip.range(4, 1)
-P1 = strip.range(9, 1)
-P2 = strip.range(8, 1)
-P3 = strip.range(7, 1)
-P4 = strip.range(6, 1)
-P5 = strip.range(5, 1)
+Led01 = strip.range(0, 1)
+Led02 = strip.range(1, 1)
+Led03 = strip.range(2, 1)
+Led04 = strip.range(3, 1)
+Led05 = strip.range(4, 1)
+Led10 = strip.range(9, 1)
+Led09 = strip.range(8, 1)
+Led08 = strip.range(7, 1)
+Led07 = strip.range(6, 1)
+Led06 = strip.range(5, 1)
+let Led02a09 = strip.range(1, 9)
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 strip.show()
+basic.pause(100)
+basic.clearScreen()
